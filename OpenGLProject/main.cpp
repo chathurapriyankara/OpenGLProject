0023ae721a80883df589911fdeca8ac8a4451c2b
chathurapriyankara <priyankarahac@gmail.com>
2020-10-07 20:42:19 +1300

    GLFW basic window

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -1,14 +1,23 @@
-//
-//  main.cpp
-//  OpenGLProject
-//
-//  Created by Chathura Priyankara on 7/10/20.
-//
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-#include <iostream>
+void renderScene();
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {//Main
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glewInit();
+    while(!glfwWindowShouldClose(window)) {
+        renderScene();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
     return 0;
+}
+
+void renderScene() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0, 0.0, 1.0, 0.0);
 }
